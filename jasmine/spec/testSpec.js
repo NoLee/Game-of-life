0e9810b349a2405b7eadcae7ecf899d0b1b53935
NoLee <manolios93@hotmail.com>
2018-11-29 17:53:07 +0200

    Added jasmine example tests

---
+++ jasmine/spec/testSpec.js
@@ -0,0 +1,20 @@
+// describe("calculatenNextCellState function", function() {
+//     it("determines if in the next generation the cell will be alive or dead", function() {
+//         expect(calculateNextCellState(3)).toEqual(6);
+//     });
+// });
+
+describe("calculateLiveNeighbors function", function() {
+
+    beforeEach(function() {
+        // game  = new GameOfLife(3,3);
+    });
+
+    it("calculates the alive neighbors of a cell", function() {
+        expect(true).toBe(true);
+        // let testArray =[[0,1,0],
+        //                 [0,1,0],
+        //                 [0,1,0]];
+        // expect(game.calculateLiveNeighbors(testArray[1][1])).toEqual(2);
+    });
+});
